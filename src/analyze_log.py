d5f2be1a637e0af86bd79a6a3de96f8837b72603
William <william.mk1@outlook.com>
2022-08-16 13:24:28 -0300

    reqs 1 e 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,52 @@
+def maria(orders):
+    most_requested = list()
+
+    for order in orders:
+        if "maria" in order:
+            food = order.split(',')[1]
+            most_requested.append(food)
+
+    return max(set(most_requested), key=most_requested.count)
+
+def arnaldo(orders):
+    food = list()
+
+    for order in orders:
+        if "arnaldo" in order:
+            line = order.split(',')[1]
+            food.append(line)
+
+    return food.count('hamburguer')
+
+def joao(orders):
+    food = list()
+    days = list()
+    joao_food = list()
+    joao_days = list()
+
+    for order in orders:
+        line = order.split(',')
+        food.append(line[1])
+        days.append(line[2].replace("\n", ""))
+        if "joao" in order:
+            joao_food.append(line[1])
+            joao_days.append(line[2].replace("\n", ""))
+
+    return [list(set(food) - set(joao_food)), list(set(days) - set(joao_days))]
+
+
 def analyze_log(path_to_file):
-    return None
+    if not (".csv") in path_to_file:
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    try:
+        with open(path_to_file, encoding='utf-8') as file:
+            orders = file.readlines()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+    with open('data/mkt_campaign.txt', 'w') as f:
+        f.write(maria(orders) + '\n')
+        f.write(str(arnaldo(orders)) + '\n')
+        f.write(str({k for k in joao(orders)[0]}) + '\n')
+        f.write(str({k for k in joao(orders)[1]}) + '\n')
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,77 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+
+    def __init__(self):
+        self.data = []
+
     def __len__(self):
-        pass
+        return len(self.data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.data.append({
+            'customer': customer,
+            'order': order,
+            'day': day
+        })
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        orders = list()
+
+        for order in self.data:
+            orders.append(order)
+
+        foods = [
+            order['order'] for order in orders if order['customer'] == customer
+        ]
+        most_ordered = Counter(foods)
+        return most_ordered.most_common()[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        orders = list()
+
+        for order in self.data:
+            orders.append(order)
+        foods = set([x['order'] for x in orders])
+        joao_orders = set(
+            [
+                order['order'] for order in orders if order['customer'] == customer
+            ]
+        )
+        never_ordered = foods - joao_orders
+
+        return never_ordered
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        orders = list()
+
+        for order in self.data:
+            orders.append(order)
+        days = set([day['day'] for day in orders])
+        joao_days = set(
+            [day['day'] for day in orders if day['customer'] == customer]
+        )
+        never_went = days - joao_days
+
+        return never_went
 
     def get_busiest_day(self):
-        pass
+        orders = list()
+
+        for order in self.data:
+            orders.append(order['day'])
+
+        days = Counter(orders)
+
+        return days.most_common()[0][0]
 
     def get_least_busy_day(self):
-        pass
+        orders = list()
+
+        for order in self.data:
+            orders.append(order['day'])
+
+        days = Counter(orders)
+
+        return days.most_common()[-1][0]
